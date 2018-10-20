e8726c8ddae12f925e8ce677cb3b6184f607fcbc
Glitch (noble-dream) <none>
2018-10-20 14:43:50 +0000

    👨‍👨‍👦🔫 Updated with Glitch

---
+++ renderer-md.js
@@ -0,0 +1,39 @@
+const marked = require('marked');
+const TerminalRenderer = require('marked-terminal');
+
+const RendererMd = (values) => {
+            
+        marked.setOptions({
+        // Define custom renderer
+        renderer: new TerminalRenderer()
+        });
+
+        let out = `# BAR MILANO #
+#### MENU DI ${values.date} ####
+
+---
+### *PRIMI*
+${values.primi.map(i => {
+    const d = i.replace('- ','');
+    return `- ${d}\n`;
+}).join('\n')}
+
+---
+### *SECONDI*
+${values.secondi.map(i => {
+    i.replace('-','');
+    return `- ${i}\n`;
+}).join('\n')}
+    
+---
+### *DOLCI*
+${values.dolci.map(i => {
+    i.replace('-','');
+    return `- ${i}\n`;
+}).join('\n')}`;
+
+        out = marked(out);
+    return out;
+};
+
+module.exports = RendererMd;
