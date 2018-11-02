69f835c944977c4329f5f01637b2169550bb4624
cobyan <yale01@gmail.com>
2018-11-02 11:43:02 +0100

    POC: app can write to slack through webhook and rtm message

---
+++ bot.js
@@ -0,0 +1,45 @@
+const { IncomingWebhook, WebClient } = require('@slack/client');
+
+// Webhook url
+const SLACK_WEBHOOK_URL = 'https://hooks.slack.com/services/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' || process.env.SLACK_WEBHOOK_URL;
+
+// An access token (from your Slack app or custom integration - xoxa, xoxp, or xoxb)
+const SLACK_TOKEN = 'xoxp-442015712583-442015712727-469628069536-44eb1bec41656e802a7640f0f2e0c56d' || process.env.SLACK_TOKEN;
+
+sendWebhook('Ciao webhook');
+sendMessage('Ciao message');
+
+function sendWebhook(msg) {
+    
+    const webhook = new IncomingWebhook(SLACK_WEBHOOK_URL);
+    
+    console.log('sending webhook to: ', SLACK_WEBHOOK_URL);
+    // Send simple text to the webhook channel
+    webhook.send(msg, function(err, res) {
+        if (err) {
+            console.log('Error:', err);
+        } else {
+            console.log('Message sent: ', res);
+        }
+    });
+}
+
+function sendMessage(msg) {
+    
+    const web = new WebClient(SLACK_TOKEN);
+    const channelIds = {
+        Yale01: {
+            general: 'CCYUFAR5G'
+        }
+    }
+    // This argument can be a channel ID, a DM ID, a MPDM ID, or a group ID
+    const conversationId = channelIds.Yale01.general;
+    
+    // See: https://api.slack.com/methods/chat.postMessage
+    web.chat.postMessage({ channel: conversationId, text: msg })
+      .then((res) => {
+        // `res` contains information about the posted message
+        console.log('Message sent: ', res.ts);
+      })
+      .catch(console.error);
+}
