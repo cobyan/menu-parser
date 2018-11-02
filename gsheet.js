b77282c5015d64480d958abe11f483f0412fdf88
cobyan <yale01@gmail.com>
2018-11-02 14:47:43 +0100

    add writeSpreadsheet

--- gsheet.js
+++ gsheet.js
@@ -1,63 +1,79 @@
 const {google} = require ('googleapis');
 const authorizedSession = require ('./lib/google-auth');
-
+/*
 const menu = {
   header: '🍇 MENÙ DI MARTEDÌ 241 OTTOBRE 🍇',
-  primi: ['primo primo', 'secondo primo', 'terzo primo'],
-  secondi: ['primo secondo', 'secondo secondo', 'terzo secondo'],
-  dolci: ['primo dolce', 'secondo dolce', 'terzo dolce'],
+  primi: ['primo primo: 6,20', 'secondo primo: 5,00', 'terzo primo: 5,20'],
+  secondi: ['primo secondo: 7,20', 'secondo secondo: 7,00', 'terzo secondo: 7,20'],
+  dolci: ['primo dolce: 4,20', 'secondo dolce: 4,20', 'terzo dolce: 4,20'],
 };
+*/
 
-let filler = (fill, length) => {
+const filler = (fill, length) => {
     const toFill = new Array(length);
     return toFill.fill(fill);
-}
-const fillLength = 29-7-menu.primi.length-menu.secondi.length-menu.dolci.length-2-2;
-
-const spreadsheetMap = {
-  'Foglio2!H2:L4': [[menu.header]],
-  'Foglio2!I7:K29': [
-    ['PRIMI', '', ''],
-    ...menu.primi.map (i => [i, null, '10']),
-    ['','',''],
-    ['SECONDI','',''],
-    ...menu.secondi.map ((i, k) => [i, null, '8']),
-    ['','',''],
-    ['DOLCI','',''],
-    ...menu.dolci.map ((i, k) => [i, null, '5']),
-    ...filler(['','',''], fillLength)
-  ],
 };
 
-const errorHandler = (err, result) => {
-  if (err) {
-    // Handle error
-    console.log (err);
-  } else {
-    console.log ('%d cells updated.', result.updatedCells);
-  }
-};
+function writeSpreadsheet(menu) {
 
-authorizedSession (printString);
+    const fillLength = 29-7-menu.primi.length-menu.secondi.length-menu.dolci.length-2-2;
 
-function printString (auth) {
-  const sheets = google.sheets ({version: 'v4', auth});
+    const row = (i) => {
+        const splitted = i.split(":");
+        return [splitted[0],"€",splitted[1]];
+    };
+    //console.log(...menu.primi.map (i => row(i)));return;
 
-  for (const range in spreadsheetMap) {
-    if (spreadsheetMap.hasOwnProperty (range)) {
-      let values = spreadsheetMap[range];
-      const resource = {
-        values,
+    const spreadsheetMap = {
+        'Foglio2!B5': [[menu.dateLong]],
+        'Foglio2!H2:L4': [[menu.header]],
+        'Foglio2!I7:K29': [
+          ['PRIMI', '', ''],
+          ...menu.primi.map (i => row(i)),
+          ['','',''],
+          ['SECONDI','',''],
+          ...menu.secondi.map ((i, k) => row(i)),
+          ['','',''],
+          ['DOLCI','',''],
+          ...menu.dolci.map ((i, k) => row(i)),
+          ...filler(['','',''], fillLength)
+        ],
       };
-      sheets.spreadsheets.values.update (
-        {
-          range,
-          resource,
-          spreadsheetId: '1_eCayZ854K9kJzqpyfY02idrO7DWVMjpu6ijuZQq9_U',
-          valueInputOption: 'USER_ENTERED',
-        },
-        errorHandler
-      );
-    }
-  }
+      
+      const errorHandler = (err, result) => {
+        if (err) {
+          // Handle error
+          console.log (err);
+        } else {
+          console.log ('%d cells updated.', result.updatedCells);
+        }
+      };
+      
+      const printString = (auth) => {
+        const sheets = google.sheets ({version: 'v4', auth});
+      
+        for (const range in spreadsheetMap) {
+          if (spreadsheetMap.hasOwnProperty (range)) {
+            let values = spreadsheetMap[range];
+            const resource = {
+              values,
+            };
+            sheets.spreadsheets.values.update (
+              {
+                range,
+                resource,
+                spreadsheetId: '1_eCayZ854K9kJzqpyfY02idrO7DWVMjpu6ijuZQq9_U',
+                valueInputOption: 'USER_ENTERED',
+              },
+              errorHandler
+            );
+          }
+        }
+      }
+      
+      authorizedSession (printString);
 }
+
+//writeSpreadsheet(menu);
+
+module.exports = writeSpreadsheet;
